b82c2ab6e02bb897b45f114345f2bf6f2056f344
Monish Gupta <monish.gupta1@gmail.com>
2016-07-07 00:52:35 +0530

    new file structure works so far.. in progress though

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'client'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'client'),
   indentedSyntax: true,
-  sourceMap: true
+  sourceMap: true,
+  debug: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('pages/error/error', {
       message: err.message,
       error: err
     });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { sample_arg: 'todos manager' });
+// });
+
+/* GET add new todo page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'todos manager' });
+  res.render('pages/addNew/addNew', { sample_arg: 'todos manager' });
 });
 
 module.exports = router;
